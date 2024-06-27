fd64762dc8056fd8f7673d8e48bfb155fbd7c760
Nikolaus J. Reichert <nikolaus@njreichert.ca>
2024-06-27 01:59:46 +0000

    Add remapping for joystick teleop.

--- src/my_bot/launch/joystick.launch.py
+++ src/my_bot/launch/joystick.launch.py
@@ -24,6 +24,7 @@ def generate_launch_description():
         executable="teleop_node",
         name="teleop_node",
         parameters=[joy_params],
+        remappings=[("/cmd_vel", "/diff_drive_controller/cmd_vel_unstamped")],
     )
 
     return LaunchDescription([
