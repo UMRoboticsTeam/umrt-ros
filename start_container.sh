adc3abc029434a5c5d1051ffea5376a4f8acccf6
Daigh Burgess <daighb@outlook.com>
2024-07-05 11:23:20 -0500

    Start container now mounts /dev

--- start_container.sh
+++ start_container.sh
@@ -5,10 +5,12 @@ docker run \
 	-it \
 	-e DISPLAY \
 	-e QT_X11_NO_MITSHM=1 \
+	--privileged \
 	--volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
 	--volume="$(pwd):/workspace" \
 	--volume="/tmp/.X11-unix/:/tmp/.X11-unix" \
 	--volume="/dev/bus/usb:/dev/bus/usb" \
+	--volume="/dev:/dev" \
 	--device-cgroup-rule="c 189:* rw" \
 	--device /dev/dri \
 	--rm \
