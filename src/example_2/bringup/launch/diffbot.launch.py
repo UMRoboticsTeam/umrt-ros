0f9344cdc17af415c8d0705617fcbb598d6dfe75
Nikolaus J. Reichert <nikolaus@njreichert.ca>
2024-07-10 03:15:40 +0000

    Add Luxonis OAK-D camera links to diffbot urdf.

--- src/example_2/bringup/launch/diffbot.launch.py
+++ src/example_2/bringup/launch/diffbot.launch.py
@@ -50,7 +50,7 @@ def generate_launch_description():
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
             PathJoinSubstitution(
-                [FindPackageShare("ros2_control_demo_example_2"), "urdf", "diffbot.urdf.xacro"]
+                [FindPackageShare("umrt_robot_description"), "description", "robot.urdf.xacro"]
             ),
             " ",
             "use_mock_hardware:=",
