1f5d5ea5204f6971bf09e1d276bcfd5eb1cb8b1a
Nikolaus J. Reichert <nikolaus@njreichert.ca>
2024-07-10 03:51:52 +0000

    launch both diffbot and camera from same file.

--- launch/main.launch.py
+++ launch/main.launch.py
@@ -1,21 +1,62 @@
 import os
 
+from pkg_resources import PkgResourcesDeprecationWarning
+
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 
 from ament_index_python import get_package_share_directory
 
 
 def generate_launch_description():
     ########################
+    # PARAMETERS
+    ########################
+    use_mock_hardware = LaunchConfiguration('use_mock_hardware')
+
+    use_mock_hardware_arg = DeclareLaunchArgument(
+        'use_mock_hardware',
+        default_value='False',
+    )
+
+    ########################
     # EXTERNAL LAUNCH FILES
     ########################
+    robot_description_launch_path = os.path.join(
+        get_package_share_directory('umrt_robot_description'),
+        'launch',
+        'robot_description.launch.py',
+    )
+
+    robot_description_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(robot_description_launch_path),
+        launch_arguments={
+            'use_mock_hardware': use_mock_hardware,
+        }.items()
+    )
+
+    drivetrain_launch_path = os.path.join(
+        get_package_share_directory('ros2_control_demo_example_2'),
+        'launch',
+        'diffbot.launch.py',
+    )
+
+    drivetrain_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(drivetrain_launch_path),
+        launch_arguments={
+            'use_mock_hardware': use_mock_hardware,
+        }.items(),
+    )
+
     camera_launch_directory = os.path.join(get_package_share_directory('depthai_examples'),
                                            'launch/mobile_publisher.launch.py')
     camera_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(camera_launch_directory)
     )
+
     ########################
     # NODE DEFINITIONS
     ########################
@@ -23,6 +64,11 @@ def generate_launch_description():
     ########################
     # LAUNCH
     ########################
-    launch_entities = [camera_launch]
+    launch_entities = [
+        use_mock_hardware_arg,
+        robot_description_launch,
+        drivetrain_launch,
+        camera_launch,
+    ]
 
     return LaunchDescription(launch_entities)
---
+++ src/umrt_robot_description/launch/robot_description.launch.py
@@ -0,0 +1,68 @@
+# Copyright 2020 ros2_control Development Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler
+from launch.conditions import IfCondition
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    # Declare arguments
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_mock_hardware",
+            default_value="false",
+            description="Start robot with mock hardware mirroring command to its states.",
+        )
+    )
+
+    use_mock_hardware = LaunchConfiguration("use_mock_hardware")
+
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare("umrt_robot_description"), "description", "robot.urdf.xacro"]
+            ),
+            " ",
+            "use_mock_hardware:=",
+            use_mock_hardware,
+        ]
+    )
+
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+        remappings=[
+            ("/diff_drive_controller/cmd_vel_unstamped", "/cmd_vel"),
+        ],
+    )
+
+    nodes = [
+        robot_state_pub_node,
+    ]
+
+    return LaunchDescription(declared_arguments + nodes)
