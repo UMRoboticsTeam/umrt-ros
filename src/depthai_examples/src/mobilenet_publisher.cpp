4422fe6e254514f4d700e929de64f08af34c3aa1
Syed Abraham Ahmed <syed.abraham14@gmail.com>
2024-07-19 18:58:22 -0500

    Update camera to mono for bandwith constraints

--- src/depthai_examples/src/mobilenet_publisher.cpp
+++ src/depthai_examples/src/mobilenet_publisher.cpp
@@ -1,4 +1,3 @@
-
 #include <cstdio>
 #include <iostream>
 
@@ -11,111 +10,81 @@
 #include "sensor_msgs/msg/image.hpp"
 #include "vision_msgs/msg/detection2_d_array.hpp"
 
-// Inludes common necessary includes for development using depthai library
+// Includes common necessary includes for development using depthai library
 #include "depthai/device/DataQueue.hpp"
 #include "depthai/device/Device.hpp"
 #include "depthai/pipeline/Pipeline.hpp"
 #include "depthai/pipeline/node/ColorCamera.hpp"
+#include "depthai/pipeline/node/MonoCamera.hpp"
 #include "depthai/pipeline/node/DetectionNetwork.hpp"
 #include "depthai/pipeline/node/XLinkOut.hpp"
 
-dai::Pipeline createPipeline(bool syncNN, std::string nnPath) {
+dai::Pipeline createPipeline() {
     dai::Pipeline pipeline;
-    auto colorCam = pipeline.create<dai::node::ColorCamera>();
+
+    auto monoCam = pipeline.create<dai::node::MonoCamera>();
     auto xlinkOut = pipeline.create<dai::node::XLinkOut>();
-    auto detectionNetwork = pipeline.create<dai::node::MobileNetDetectionNetwork>();
-    auto nnOut = pipeline.create<dai::node::XLinkOut>();
 
-    xlinkOut->setStreamName("preview");
-    nnOut->setStreamName("detections");
+    xlinkOut->setStreamName("mono");
 
-    colorCam->setPreviewSize(300, 300);
-    colorCam->setResolution(dai::ColorCameraProperties::SensorResolution::THE_720_P);
-    colorCam->setInterleaved(false);
-    colorCam->setColorOrder(dai::ColorCameraProperties::ColorOrder::BGR);
-    colorCam->setFps(5);
+    monoCam->setResolution(dai::MonoCameraProperties::SensorResolution::THE_400_P);
+    monoCam->setFps(1);
+    monoCam->setImageOrientation(dai::CameraImageOrientation::NORMAL);
+    monoCam->setIsp3aFps(0); //default is 0 anyway but added for redundancy
+    monoCam->setRawOutputPacked(true);
 
-    // testing MobileNet DetectionNetwork
-    detectionNetwork->setConfidenceThreshold(0.5f);
-    detectionNetwork->setBlobPath(nnPath);
+    monoCam->out.link(xlinkOut->input);
 
-    // Link plugins CAM -> NN -> XLINK
-    colorCam->preview.link(detectionNetwork->input);
-    if(syncNN)
-        detectionNetwork->passthrough.link(xlinkOut->input);
-    else
-        colorCam->preview.link(xlinkOut->input);
-
-    detectionNetwork->out.link(nnOut->input);
     return pipeline;
 }
 
 int main(int argc, char** argv) {
     rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("mobilenet_node");
+    auto node = rclcpp::Node::make_shared("mono_camera_node");
 
     std::string tfPrefix, resourceBaseFolder, nnPath;
     std::string cameraParamUri = "package://depthai_examples/params/camera";
-    std::string nnName(BLOB_NAME);
-    bool syncNN;
+
     int bad_params = 0;
 
     node->declare_parameter("tf_prefix", "oak");
     node->declare_parameter("camera_param_uri", cameraParamUri);
     node->declare_parameter("resourceBaseFolder", "");
-    node->declare_parameter("sync_nn", syncNN);
-    node->declare_parameter<std::string>("nnName", "");
 
     node->get_parameter("tf_prefix", tfPrefix);
     node->get_parameter("camera_param_uri", cameraParamUri);
-    node->get_parameter("sync_nn", syncNN);
     node->get_parameter("resourceBaseFolder", resourceBaseFolder);
 
     if(resourceBaseFolder.empty()) {
-        throw std::runtime_error("Send the path to the resouce folder containing NNBlob in \'resourceBaseFolder\' ");
+        throw std::runtime_error("Send the path to the resource folder containing NNBlob in 'resourceBaseFolder'");
     }
     // Uses the path from param if passed or else uses from BLOB_PATH from CMAKE
-    std::string nnParam;
-    node->get_parameter("nnName", nnParam);
-    if(nnParam != "x") {
-        node->get_parameter("nnName", nnName);
-    }
 
-    nnPath = resourceBaseFolder + "/" + nnName;
-    dai::Pipeline pipeline = createPipeline(syncNN, nnPath);
+    dai::Pipeline pipeline = createPipeline();
     dai::Device device(pipeline);
 
-    std::shared_ptr<dai::DataOutputQueue> previewQueue = device.getOutputQueue("preview", 30, false);
-    std::shared_ptr<dai::DataOutputQueue> nNetDataQueue = device.getOutputQueue("detections", 30, false);
+    std::shared_ptr<dai::DataOutputQueue> monoQueue = device.getOutputQueue("mono", 30, false);
 
-    std::string color_uri = cameraParamUri + "/" + "color.yaml";
+    std::string mono_uri = cameraParamUri + "/" + "mono.yaml";
 
-    // TODO(sachin): Add option to use CameraInfo from EEPROM
-    dai::rosBridge::ImageConverter rgbConverter(tfPrefix + "_rgb_camera_optical_frame", false);
-    dai::rosBridge::BridgePublisher<sensor_msgs::msg::Image, dai::ImgFrame> rgbPublish(previewQueue,
-                                                                                       node,
-                                                                                       std::string("color/image"),
-                                                                                       std::bind(&dai::rosBridge::ImageConverter::toRosMsg,
-                                                                                                 &rgbConverter,  // since the converter has the same frame name
-                                                                                                                 // and image type is also same we can reuse it
-                                                                                                 std::placeholders::_1,
-                                                                                                 std::placeholders::_2),
-                                                                                       30,
-                                                                                       color_uri,
-                                                                                       "color");
+    dai::rosBridge::ImageConverter monoConverter(tfPrefix + "_mono_camera_optical_frame", false);
+    dai::rosBridge::BridgePublisher<sensor_msgs::msg::Image, dai::ImgFrame> monoPublish(monoQueue,
+                                                                                        node,
+                                                                                        std::string("mono/image"),
+                                                                                        std::bind(&dai::rosBridge::ImageConverter::toRosMsg,
+                                                                                                  &monoConverter,  // since the converter has the same frame name
+                                                                                                                  // and image type is also same we can reuse it
+                                                                                                  std::placeholders::_1,
+                                                                                                  std::placeholders::_2),
+                                                                                        30,
+                                                                                        mono_uri,
+                                                                                        "mono");
 
-    dai::rosBridge::ImgDetectionConverter detConverter(tfPrefix + "_rgb_camera_optical_frame", 300, 300, false);
-    dai::rosBridge::BridgePublisher<vision_msgs::msg::Detection2DArray, dai::ImgDetections> detectionPublish(
-        nNetDataQueue,
-        node,
-        std::string("color/mobilenet_detections"),
-        std::bind(&dai::rosBridge::ImgDetectionConverter::toRosMsg, &detConverter, std::placeholders::_1, std::placeholders::_2),
-        30);
-
-    detectionPublish.addPublisherCallback();
-    rgbPublish.addPublisherCallback();  // addPublisherCallback works only when the dataqueue is non blocking.
+    monoPublish.addPublisherCallback();  // addPublisherCallback works only when the dataqueue is non blocking.
 
     rclcpp::spin(node);
 
     return 0;
 }
+
+
