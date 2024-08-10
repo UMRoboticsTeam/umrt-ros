7fd87c8ae93843ee1d2a9b39e41efb97004512cc
Nikolaus J. Reichert <nikolaus@njreichert.ca>
2024-08-10 14:36:33 -0600

    export ROS_DOMAIN_ID=8 by default to solve conflict with arm

--- start_container.sh
+++ start_container.sh
@@ -5,6 +5,7 @@ docker run \
 	-it \
 	-e DISPLAY \
 	-e QT_X11_NO_MITSHM=1 \
+	-e ROS_DOMAIN_ID=8 \
 	--privileged \
 	--volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
 	--volume="$(pwd):/workspace" \
